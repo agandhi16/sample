ed3cccf6135a58a94092a6727f875f75dde809af
agandhi16 <alex_gandhi@asl.org>
2014-10-10 10:46:52 +0100

    Hello2.java
    Edits of Original Code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, Github");
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Hello, Github User");
+    System.out.println("What is your name?");
+    String name = scn.next();
+    System.out.println("Hello, " +name);
+    System.out.println("When was your birth year?");
+    double year = scn.nextDouble();
+    if (year >=1995) {
+    System.out.println("You are not old enough for this program, goodbye.");
+                     }
+    else {
+    System.out.println("Welcome to the program, how can I help you?");
+    String answer1 = scn.next();
+    System.out.println("Error 404, program does not support " +answer1);
+         }
+   
   }
 }
